8b83c4f2a19a7b37ad7ab54d01a93a09b7d5c73e
abyyazhari-hub <abyyazhari@gmail.com>
2026-08-07 07:08:20 +0700

    Delete src directory

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-    STORE_NAME: process.env.STORE_NAME || "pummarket.id",
-    OWNER_NAME: process.env.OWNER_NAME || "Pum",
-    PREFIX: process.env.PREFIX || ".",
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
-    PORT: process.env.PORT || 3000
-};
