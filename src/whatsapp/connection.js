2e7f584e1d90ee87e13baf6cb0ada58b7deb3419
abyyazhari-hub <abyyazhari@gmail.com>
2026-08-07 07:32:58 +0700

    Update connection.js

--- src/whatsapp/connection.js
+++ src/whatsapp/connection.js
@@ -1,1 +1,28 @@
+const {
+    default: makeWASocket,
+    DisconnectReason,
+    useMultiFileAuthState,
+    fetchLatestBaileysVersion
+} = require("@whiskeysockets/baileys");
 
+const { Boom } = require("@hapi/boom");
+const logger = require("../lib/logger");
+const config = require("../config/config");
+
+async function startBot() {
+
+    const { state, saveCreds } = await useMultiFileAuthState("./auth");
+
+    const { version } = await fetchLatestBaileysVersion();
+
+    const sock = makeWASocket({
+        version,
+        auth: state,
+        printQRInTerminal: true
+    });
+
+    sock.ev.on("creds.update", saveCreds);
+
+}
+
+module.exports = startBot;
