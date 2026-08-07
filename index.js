8a0a8fbfeea36dade69eacba5e1bd829ce702e63
abyyazhari-hub <abyyazhari@gmail.com>
2026-08-07 07:09:33 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+console.log("================================");
+console.log("      PUMMARKET.ID BOT");
+console.log("================================");
+console.log("Bot Starting...");
+console.log("Store :", process.env.STORE_NAME);
+console.log("Owner :", process.env.OWNER_NAME);
