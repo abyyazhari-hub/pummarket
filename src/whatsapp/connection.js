ee00d45ed4b2af8f226dae0eaf95a70b647b557a
abyyazhari-hub <abyyazhari@gmail.com>
2026-08-07 07:35:39 +0700

    Update connection.js

--- src/whatsapp/connection.js
+++ src/whatsapp/connection.js
@@ -1,13 +1,12 @@
 const {
     default: makeWASocket,
-    DisconnectReason,
     useMultiFileAuthState,
-    fetchLatestBaileysVersion
+    fetchLatestBaileysVersion,
+    DisconnectReason
 } = require("@whiskeysockets/baileys");
 
 const { Boom } = require("@hapi/boom");
 const logger = require("../lib/logger");
-const config = require("../config/config");
 
 async function startBot() {
 
@@ -22,6 +21,31 @@ async function startBot() {
     });
 
     sock.ev.on("creds.update", saveCreds);
+
+    sock.ev.on("connection.update", ({ connection, lastDisconnect }) => {
+
+        if (connection === "close") {
+
+            const shouldReconnect =
+                (lastDisconnect?.error instanceof Boom
+                    ? lastDisconnect.error.output.statusCode
+                    : 0) !== DisconnectReason.loggedOut;
+
+            logger.info("Disconnected");
+
+            if (shouldReconnect) {
+                startBot();
+            }
+
+        } else if (connection === "open") {
+
+            logger.info("==========================");
+            logger.info("PUMMARKET BOT CONNECTED");
+            logger.info("==========================");
+
+        }
+
+    });
 
 }
 
