531ea7298412896e1a89f0dc52356291d5e574ab
abyyazhari-hub <abyyazhari@gmail.com>
2026-08-07 07:15:07 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 
 module.exports = {
-  store: process.env.STORE_NAME,
-  owner: process.env.OWNER_NAME,
-  prefix: process.env.PREFIX,
-  openai: process.env.OPENAI_API_KEY
+    store: process.env.STORE_NAME || "pummarket.id",
+    owner: process.env.OWNER_NAME || "pum",
+    prefix: process.env.PREFIX || ".",
+    openai: process.env.OPENAI_API_KEY || "",
+    port: process.env.PORT || 3000
 };
